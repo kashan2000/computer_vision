9216473cb65458dd523d9a586c8fcea5b7b9be90
Kashan Khan <epe18018@rgipt.ac.in>
2024-08-07 13:42:57 +0530

    debug

--- model_config.py
+++ model_config.py
@@ -89,7 +89,7 @@ def get_pose_from_frame(frame):
 
                     if len(pr.keypoints.xyn) > 0:
                         print(f"value of idx> {idx}")
-                        print(f"length of list is {len(pr.keypoints.xyn)}")
+                        print(f"length of list is {len(pr.keypoints.xyn[0])}")
                         keypoint_list = pr.keypoints.xyn[0][idx].tolist()
                         confidence = pr.keypoints.conf[0][idx] if hasattr(pr.keypoints, 'conf') else 1.0
                         print(f"Detected {key}: {keypoint_list} with confidence {confidence}")
